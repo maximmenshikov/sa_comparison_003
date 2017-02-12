fd041b2cd40d9163a5b2a445f9c25b128f5a3a95
Maxim Menshchikov <maximmenshchikov@gmail.com>
2017-02-12 14:06:44 +0300

    Script to perform comparison

---
+++ run.sh
@@ -0,0 +1,65 @@
+#!/bin/sh
+# Script to perform comparison of few static analysis utilities on
+# Toyota ITC benchmarks.
+# (C) Maxim Menshchikov 2017
+
+git clone https://github.com/mmenshchikov/itc-benchmarks.git
+
+root=$(pwd)
+bench=${root}/itc-benchmarks
+
+# Run cppcheck
+rm "${root}/result_cppcheck.txt"
+cd ${bench}/01.w_Defects
+cppcheck . 2>> "${root}/result_cppcheck.txt"
+cd ${bench}/02.wo_Defects
+cppcheck . 2>> "${root}/result_cppcheck.txt"
+
+# Run Clang
+rm -rf "${root}/result_clang"
+cd $bench
+./bootstrap
+./configure
+make clean
+mkdir "${root}/result_clang"
+scan-build-3.9 -v -o "${root}/result_clang" make
+
+# Run Frama-C
+cd ${bench}/01.w_Defects
+frama-c -cpp-extra-args="-I../include -isystem $(frama-c -print-share-path)/libc -isystem /usr/include -include ${root}/gnuc_prereq.h -D__FC_DEFINE_PID_T" \
+        -val $(ls *.c | grep -v "extern_1" | grep -v "invalid_memory_access.c" | grep -v "st_overflow.c" | grep -v "st_underrun.c" | xargs) \
+        >> ${root}/result_frama-c.txt
+cd ${bench}/02.wo_Defects
+frama-c -cpp-extra-args="-I../include -isystem $(frama-c -print-share-path)/libc -isystem /usr/include -include ${root}/gnuc_prereq.h -D__FC_DEFINE_PID_T" \
+        -val $(ls *.c | grep -v "extern_1" | grep -v "invalid_memory_access.c" | grep -v "st_overflow.c" | grep -v "st_underrun.c" | xargs) \
+        >> ${root}/result_frama-c.txt
+
+# Run CLion
+rm -rf ${root}/result_clion
+if [[ -z ${CLION_DIR} ]] ; then
+CLION_DIR="/opt/clion-2016.3.2"
+fi
+${CLION_DIR}/bin/inspect.sh ${bench} Project_Default ${root}/result_clion -v2
+
+# Build PVS-Studio comment inserting utility
+cd ${root}
+git clone https://github.com/viva64/how-to-use-pvs-studio-free.git
+cd how-to-use-pvs-studio-free
+mkdir build
+cd build
+pvs_root=$(pwd)
+cmake -DCMAKE_BUILD_TYPE=Release ..
+make
+cd ${root}
+cp -R itc-benchmarks ./pvs_code
+cd pvs_code
+make clean
+
+# Insert comments
+${pvs_root}/how-to-use-pvs-studio-free -c 1 .
+
+# Analyze
+mkdir -p "${root}/result_pvs"
+pvs-studio-analyzer trace -o "${root}/result_pvs/strace_out" -- make
+pvs-studio-analyzer analyze -f "${root}/result_pvs/strace_out" -o "${root}/result_pvs/project.log"
+plog-converter -a GA:1,2 -t tasklist -o "${root}/result_pvs/project.tasks" "${root}/result_pvs/project.log"
