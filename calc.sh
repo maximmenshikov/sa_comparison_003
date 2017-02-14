db3c888e394c1b09c7a10dbcc5fa76c595237c7e
Maxim Menshchikov <maximmenshchikov@gmail.com>
2017-02-14 13:37:26 +0300

    Rename scored to evaluated

--- calc.sh
+++ calc.sh
@@ -9,12 +9,12 @@ parsers=${root}/sa_parsers
 
 echo Results > result_final.txt
 echo Clang >> result_final.txt
-${parsers}/report_calc/bin/Debug/report_calc.exe 03.scored/clang_scored.txt ${bench} >> result_final.txt
+${parsers}/report_calc/bin/Debug/report_calc.exe 03.evaluated/clang_evaluated.txt ${bench} >> result_final.txt
 echo Cppcheck >> result_final.txt
-${parsers}/report_calc/bin/Debug/report_calc.exe 03.scored/cppcheck_scored.txt ${bench} >> result_final.txt
+${parsers}/report_calc/bin/Debug/report_calc.exe 03.evaluated/cppcheck_evaluated.txt ${bench} >> result_final.txt
 echo Frama-C >> result_final.txt
-${parsers}/report_calc/bin/Debug/report_calc.exe 03.scored/framac_scored.txt ${bench} >> result_final.txt
+${parsers}/report_calc/bin/Debug/report_calc.exe 03.evaluated/framac_evaluated.txt ${bench} >> result_final.txt
 echo PVS Studio >> result_final.txt
-${parsers}/report_calc/bin/Debug/report_calc.exe 03.scored/pvs_scored.txt ${bench} >> result_final.txt
+${parsers}/report_calc/bin/Debug/report_calc.exe 03.evaluated/pvs_evaluated.txt ${bench} >> result_final.txt
 echo Resharper >> result_final.txt
-${parsers}/report_calc/bin/Debug/report_calc.exe 03.scored/resharper_scored.txt ${bench} >> result_final.txt
+${parsers}/report_calc/bin/Debug/report_calc.exe 03.evaluated/resharper_evaluated.txt ${bench} >> result_final.txt
